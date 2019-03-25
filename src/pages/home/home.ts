effec895e3ab74b17a370532b315c0dcb31faeb7
name <email@email.com>
2019-03-25 14:57:29 -0600

    actividad 1 API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  casas = [];
+  constructor(public navCtrl: NavController,public http:HttpClient) {
+    this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15').subscribe(data =>{
+      this.casas=data['list_ads'];
+      console.log(JSON.stringify(this.casas));
+    },error => {
+      console.log(JSON.stringify(error));
+    });
 
   }
 
